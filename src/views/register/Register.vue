92e801527374bb60b691f8d7edf91e81031dfd80
Adan CCR <mglatinos@gmail.com>
2020-05-02 02:57:17 -0500

    tets

--- src/views/register/Register.vue
+++ src/views/register/Register.vue
@@ -1,61 +1,15 @@
 <template>
   <div class="view-register">
-    <div class="container-register">
-      <div class="container-first">
-        <header>
-          <div class="first">
-            <img src="./Img/Pacha logo.png" height="30px" alt />
-          </div>
-        </header>
-        <main>
-          <div class="picture first">
-            <img src="./Img/undraw_researching_22gp (1).svg" alt />
-          </div>
-        </main>
-      </div>
-
-      <div class="container-second">
-        <header>
-          <div class="second">
-            <h1>Bienvenid@</h1>
-          </div>
-        </header>
-        <main>
-          <section class="formulario">
-            <p>NOMBRE Y APELLIDO</p>
-            <div class="input two">
-              <input type="text" name id placeholder="Escribe aqui..." />
-            </div>
-            <P>EDAD</P>
-            <div class="input two">
-              <input type="text" name id placeholder="Escribe aqui..." />
-            </div>
-            <P>CELULAR</P>
-            <div class="input two">
-              <input type="text" name id placeholder="Escribe aqui..." />
-            </div>
-            <P>SEDE</P>
-            <div class="input two">
-              <input type="text" name id placeholder="Escribe aqui..." />
-            </div>
-            <P>PROGRAMA DE ESPECIALIZACION</P>
-            <div class="input two">
-              <input type="text" name id placeholder="Escribe aqui..." />
-            </div>
-
-            <button class="btn2 state3">Ingresar</button>
-          </section>
-        </main>
-      </div>
-    </div>
+    <h1>Hola Register!</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Register"
-};
+  name: 'Register',
+}
 </script>
 
 <style lang="">
+
 </style>
